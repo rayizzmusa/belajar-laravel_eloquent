5db5ab1e12dbd29d73d59b0685281b8b5a2502b5
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-02 14:10:12 +0700

    membuat uuid selain primary key, nambah soft delete

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Voucher extends Model
 {
-    use HasUlids;
+    use HasUlids, SoftDeletes;
 
     protected $table = "vouchers";
     protected $primaryKey = "id";

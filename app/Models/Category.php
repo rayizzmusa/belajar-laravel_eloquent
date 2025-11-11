ef03018ad62cd3be2deddfb0430c4db2db8424bd
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-11 20:06:01 +0700

    fillable, Global Scope, has many, has one of many

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use App\Models\Scopes\IsActiveScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Category extends Model
 {

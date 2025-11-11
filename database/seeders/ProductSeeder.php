9347fecbc60d903be23e3e03f57341f45d1d3db2
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-11 20:07:51 +0700

    updated product has one of many

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,5 +19,13 @@ class ProductSeeder extends Seeder
         $product->description = "desc product 1";
         $product->category_id = "FOOD";
         $product->save();
+
+        $product2 = new Product();
+        $product2->id = "2";
+        $product2->name = "Prod 2";
+        $product2->description = "desc product 2";
+        $product2->category_id = "FOOD";
+        $product2->price = 300;
+        $product2->save();
     }
 }

02ba8ce99528a300e36f79ed5ae0c9ce60975fc0
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-12 20:59:54 +0700

    model reviews

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    use HasFactory;
+    protected $table = "reviews";
+    protected $keyType = 'int';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = false;
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, "product_id", "id");
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, "customer_id", "id");
+    }
 }

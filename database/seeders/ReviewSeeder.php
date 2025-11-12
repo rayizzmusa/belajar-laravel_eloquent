7e4caddffcd0bbda7d42492f95687333c10c1b99
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-12 21:09:16 +0700

    reviews seeder

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Review;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,18 @@ class ReviewSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $review = new Review();
+        $review->product_id = "1";
+        $review->customer_id = "SYIFA";
+        $review->rating = 5;
+        $review->comment = "Menarik";
+        $review->save();
+
+        $review = new Review();
+        $review->product_id = "2";
+        $review->customer_id = "SYIFA";
+        $review->rating = 4;
+        $review->comment = "Byuhh";
+        $review->save();
     }
 }

d26d9f05400a67023d1ea69b60cfa5f8d1a78620
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-14 21:20:12 +0700

    one to one relationship, one to one query, has one through, many to many

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -4,7 +4,9 @@ namespace Tests\Feature;
 
 use App\Models\Customer;
 use App\Models\Wallet;
+use Database\Seeders\CategorySeeder;
 use Database\Seeders\CustomerSeeder;
+use Database\Seeders\ProductSeeder;
 use Database\Seeders\VirtualAccountSeeder;
 use Database\Seeders\WalletSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -64,5 +66,19 @@ class CustomerTest extends TestCase
         $virtualAccount = $customer->VirtualAccount;
         self::assertNotNull($virtualAccount);
         self::assertEquals("Mandiri", $virtualAccount->bank);
+    }
+
+    public function testManyToMany()
+    {
+        $this->seed([CustomerSeeder::class, CategorySeeder::class, ProductSeeder::class]);
+
+        $customer = Customer::find("SYIFA");
+        self::assertNotNull($customer);
+
+        $customer->likeProducts()->attach("1"); //products id
+
+        $products = $customer->likeProducts;
+        self::assertCount(1, $products);
+        self::assertEquals("1", $products[0]->id);
     }
 }

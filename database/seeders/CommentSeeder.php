935cbc8b814e7de0ef99595b41750c079f748e6e
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-16 15:08:42 +0700

    one to many relation, one to many query, polymorphic relations one to one & one to many

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Product;
+use App\Models\Voucher;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,31 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->createCommentsForProducts();
+        $this->createCommentsForVouchers();
+    }
+
+    public function createCommentsForProducts()
+    {
+        $product = Product::find("1");
+
+        $comment = new Comment();
+        $comment->email = "ray@exc.com";
+        $comment->title = "title";
+        $comment->commentable_id = $product->id;
+        $comment->commentable_type = Product::class;
+        $comment->save();
+    }
+
+    public function createCommentsForVouchers()
+    {
+        $voucher = Voucher::first();
+
+        $comment = new Comment();
+        $comment->email = "ray@exc.com";
+        $comment->title = "title";
+        $comment->commentable_id = $voucher->id;
+        $comment->commentable_type = Voucher::class;
+        $comment->save();
     }
 }
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -5,8 +5,10 @@ namespace Tests\Feature;
 use App\Models\Category;
 use App\Models\Product;
 use Database\Seeders\CategorySeeder;
+use Database\Seeders\CommentSeeder;
 use Database\Seeders\ImageSeeder;
 use Database\Seeders\ProductSeeder;
+use Database\Seeders\VoucherSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -63,5 +65,18 @@ class ProductTest extends TestCase
 
         $image = $product->image;
         self::assertEquals("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.smartsurvey.co.uk%2Fblog%2Fproduct-concept-what-is-it-how-to-use-it&psig=AOvVaw3eHRWLzmJy2R2hJ79HoaT6&ust=1763362692286000&source=images&cd=vfe&opi=89978449&ved=0CBcQjRxqFwoTCIDA3oiM9pADFQAAAAAdAAAAABAE", $image->url);
+    }
+
+    public function testOneToManyPolymorphic()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class, VoucherSeeder::class, CommentSeeder::class]);
+
+        $product = Product::query()->first();
+        $comments = $product->comments;
+        self::assertCount(1, $comments);
+        foreach ($comments as $comment) {
+            self::assertEquals(Product::class, $comment->commentable_type);
+            self::assertEquals($product->id, $comment->commentable_id);
+        }
     }
 }

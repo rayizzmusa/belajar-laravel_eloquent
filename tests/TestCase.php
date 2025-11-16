1a8c44dfae6f8949eeab500546339bf31efd1f89
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-16 14:01:57 +0700

    update test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,6 +19,7 @@ abstract class TestCase extends BaseTestCase
         DB::delete("delete from wallets");
         DB::delete("delete from reviews");
         DB::delete("delete from customers_likes_products");
+        DB::delete("delete from images");
         DB::delete("delete from products");
         DB::delete("delete from customers");
         DB::delete("delete from categories");

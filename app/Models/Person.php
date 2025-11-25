1f2ab427dd7c572148ea918b31accb34c761817f
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-25 20:32:44 +0700

    model persons to accessor and mutator, accessor & mutators nama sama dengan kolom, attribute casting

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -14,6 +14,11 @@ class Person extends Model
     public $incrementing = true;
     public $timestamps = true;
 
+    protected $casts = [
+        "created_at" => "datetime",
+        "update_at" => "datetime"
+    ];
+
     protected function fullName(): Attribute
     {
         return Attribute::make(

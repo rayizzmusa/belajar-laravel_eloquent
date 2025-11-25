5df0e4202e61254d4ee931f4dd06346ca95dcf4c
Rayhan Muhammad <rayhanmm1234@gmail.com>
2025-11-25 20:59:09 +0700

    add migration update address persons

--- database/migrations/2025_11_25_135529_add_column_address_to_persons.php
+++ database/migrations/2025_11_25_135529_add_column_address_to_persons.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('persons', function (Blueprint $table) {
-            //
+            $table->string("address", 500)->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('persons', function (Blueprint $table) {
-            //
+            $table->dropColumn("address");
         });
     }
 };
